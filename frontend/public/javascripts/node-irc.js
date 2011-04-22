6b42967b3ca6d7b48bc586443b48b8a3352843c7
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-04-22 20:23:49 +0800

    add messages and user list.

--- frontend/public/javascripts/node-irc.js
+++ frontend/public/javascripts/node-irc.js
@@ -1,5 +1,5 @@
 $(function(){
-	$("button").button();
+	$("button, div.users > ul > li > a").button();
 
 	var $window = $(window);
 	var $ui_container = $('#ui-container');
@@ -7,6 +7,18 @@ $(function(){
 	var $settings_sidebar_container = $('#settings-sidebar-container');
 	var $contents_container = $('#contents-container');
 	var $channel_container = $('#channel-container');
+	var $channel_tabs = $('#channel-tabs');
+	$channel_tabs.tabs({
+		select: function(event, ui) {
+			var $tab = $(ui.tab);
+			if ($tab.attr('id') != 'channel-add') {
+				return true;
+			}
+
+			console.log('channel-add');
+			return false;
+		}
+	});
 	var $say_container = $('#say-container');
 	var $say = $('#say');
 
@@ -30,7 +42,28 @@ $(function(){
 
 		var scw = $say_container.outerWidth();
 		$say.width(scw - 100);
+
+		$channel_tabs.height($channel_container.innerHeight() - 8);
+
+		$('div.tabs-contents').each(function(){
+			var $contents = $(this);
+			var $messages = $('div.messages', $contents);
+			var $users = $('div.users', $contents);
+			var uw = $users.outerWidth();
+			//console.log("width: %s, outerWidth: %s, innerWidth: %s", $contents.width(), $contents.outerWidth(), $contents.innerWidth());
+			$messages.width(~~($contents.width() - uw));
+
+			var $tabs = $('ul.ui-tabs-nav', $channel_tabs);
+			var contents_offset = $contents.outerHeight() - $contents.height();
+			var cth = $channel_tabs.height();
+			console.log("tabs.height: %s, contents_offset: %s, cth: %s", $tabs.height(), contents_offset, cth);
+			var ch = cth - $tabs.outerHeight() - contents_offset;
+			$contents.height(ch);
+			$messages.height(ch);
+			$users.height(ch);
+		});
 	};
+
 	resize_ui();
 	$window.bind("resize", function(){
 		resize_ui();
